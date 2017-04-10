47868b70009f32f53ef670321f7d3b960b6e7a5f
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-04-10 20:55:42 +0200

    chore(persefone): add configuration files

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import io
+from setuptools import find_packages, setup
+
+
+readme = io.open('README.rst', 'r', encoding='utf-8').read()
+
+setup(
+    name='persefone',
+    description='Encoder for the Siren hypermedia format',
+    long_description=readme,
+    url='https://github.com/Vesuvium/persefone',
+    author='Jacopo Cascioli',
+    author_email='jacopocascioli@gmail.com',
+    license='MIT',
+    version='0.0.0',
+    packages=find_packages(),
+    tests_require=[
+        'pytest',
+    ],
+    setup_requires=['pytest-runner'],
+    install_requires=[],
+    classifiers=[]
+)
