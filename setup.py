337973b01b3bfdc7db2a63308f64b35faa159807
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-11-18 18:50:46 +0100

    chore(setup): version bump to 0.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author='Jacopo Cascioli',
     author_email='jacopocascioli@gmail.com',
     license='MIT',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     tests_require=[
         'pytest',
