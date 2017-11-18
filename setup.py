ab5c70992b58ec91d55adf88d7b70022f52815ca
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-11-18 11:36:22 +0100

    chore(setup): add support for publish command

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 import io
+import os
+import sys
 
 from setuptools import find_packages, setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
 
 
 readme = io.open('README.rst', 'r', encoding='utf-8').read()
