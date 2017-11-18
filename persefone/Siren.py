08ec92a16cdb61d5b153ab81fa4649edf925f2a7
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-11-18 11:17:58 +0100

    feat(siren): add Siren encoder

--- persefone/Siren.py
+++ persefone/Siren.py
@@ -1,5 +1,65 @@
 # -*- coding: utf-8 -*-
+import ujson
+
+
 class Siren:
 
+    def __init__(self, data, path, model=None, total_items=0, current_page=1):
+        self.data = data
+        self.path = path
+        self.model = model
+        self.total_items = total_items
+        self.current_page = current_page
+
+    def paginate(self):
+        """
+        Generates pagination metadata
+        """
+        links = [
+            {'rel': ['self'], 'href': self.path}
+        ]
+
+        if self.total_items > len(self.data):
+            href = '{}?page={}'.format(self.path, self.current_page + 1)
+            links.append({'rel': ['next'], 'href': href})
+
+        if self.current_page != 1:
+            href = '{}?page={}'.format(self.path, self.current_page - 1)
+            links.append({'rel': ['previous'], 'href': href})
+        return links
+
+    def make_entity(self):
+        """
+        Produces a Siren document with a single data object.
+        """
+        href = '{}/{}'.format(self.path, self.data.id)
+        return {
+            'properties': self.data._data,
+            'class': [self.data.__class__.__name__],
+            'links': [
+                {'href': href, 'rel': 'self'}
+            ]
+        }
+
+    def make_entities(self):
+        entities = []
+        for item in self.data:
+            entities.append(self.make_entity(item))
+
+        fields = []
+        name = 'add-item'
+        if self.model:
+            fields = self.model.get_columns()
+            name = 'add-' + self.model._meta.name
+
+        actions = [
+            {'name': name, 'method': 'POST', 'type': 'application/json',
+             'fields': fields}
+        ]
+        links = self.paginate()
+        return {'entities': entities, 'actions': actions, 'links': links}
+
     def encode(self):
-        return {}
+        if type(self.data) == list:
+            return ujson.dumps(self.make_entities())
+        return ujson.dumps(self.make_entity(self.data))
--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,74 @@
 # -*- coding: utf-8 -*-
-import pytest
+from unittest.mock import MagicMock
+
 from persefone import Siren
 
-def test_siren_encode():
-    siren = Siren()
-    assert type(siren.encode()) == dict
+from pytest import fixture
+
+import ujson
+
+
+@fixture
+def item():
+    return MagicMock()
+
+
+@fixture
+def siren(item):
+    return Siren(item, '/path')
+
+
+@fixture
+def siren_with_list(item):
+    return Siren([item], '/path')
+
+
+def test_paginate():
+    links = Siren([], 'path', total_items=1, current_page=1).paginate()
+    assert links[0] == {'rel': ['self'], 'href': 'path'}
+    assert links[1] == {'href': 'path?page=2', 'rel': ['next']}
+
+
+def test_paginate_middle():
+    links = Siren([], 'path', total_items=1, current_page=2).paginate()
+    assert links[0] == {'href': 'path', 'rel': ['self']}
+    assert links[1] == {'href': 'path?page=3', 'rel': ['next']}
+    assert links[2] == {'href': 'path?page=1', 'rel': ['previous']}
+
+
+def test_paginate_last():
+    links = Siren([], 'path', total_items=0, current_page=3).paginate()
+    assert links[0] == {'rel': ['self'], 'href': 'path'}
+    assert links[1] == {'rel': ['previous'], 'href': 'path?page=2'}
+
+
+def test_make_entity(siren, item):
+    entity = siren.make_entity()
+    assert entity['properties'] == item._data
+    assert entity['class'] == [item.__class__.__name__]
+    assert entity['links'] == [
+        {'href': '/path/{}'.format(item.id), 'rel': 'self'}
+    ]
+
+
+def test_make_entities(mocker, siren_with_list):
+    mocker.patch.object(Siren, 'make_entity')
+    mocker.patch.object(Siren, 'paginate')
+    entities = siren_with_list.make_entities()
+    assert entities['entities'] == [Siren.make_entity()]
+    assert type(entities['actions']) == list
+    assert entities['links'] == Siren.paginate()
+
+
+def test_encode(mocker, siren_with_list):
+    mocker.patch.object(Siren, 'make_entities')
+    mocker.patch.object(ujson, 'dumps')
+    siren_with_list.encode()
+    ujson.dumps.assert_called_with(Siren.make_entities())
+
+
+def test_encode_one(mocker, siren):
+    mocker.patch.object(Siren, 'make_entity')
+    mocker.patch.object(ujson, 'dumps')
+    siren.encode()
+    ujson.dumps.assert_called_with(Siren.make_entity())
